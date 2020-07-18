fabe6d7128caac8291aebc135349105ce1c0cf69
Ravikesh Yadav <ravikesh@netzary.com>
2020-07-18 14:33:13 +0530

    new design

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,68 +1,39 @@
- // pickup map
-  var locations = [
-    ['Sausalito', 37.8590937, -122.4852507, 'assets/img/delivery-location.png'],
-    ['Sacramento', 38.5815719, -121.4943996, 'assets/img/pick-location.jpg'],
-  ];
-  var map = new google.maps.Map(document.getElementById('pickupmap'), {
-    zoom: 6,
-    center: {
-      lat: 37.8590937,
-      lng: -122.4852507
-    },
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-  var infowindow = new google.maps.InfoWindow();
-  var marker, i;
-  for (i = 0; i < locations.length; i++) {
-    marker = new google.maps.Marker({
-      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-      map: map,
-      icon: locations[i][3]
-    });
-    google.maps.event.addListener(marker, 'click', (function (marker, i) {
-      return function () {
-        infowindow.setContent(locations[i][0]);
-        infowindow.open(map, marker);
-      }
-    })(marker, i));
-  }
-  var line = new google.maps.Polyline({
-    path: [new google.maps.LatLng(37.8590937, -122.4852507), new google.maps.LatLng(38.5815719, -121.4943996)],
-    strokeColor: "#00a000",
-    strokeOpacity: 1.0,
-    strokeWeight: 5,
-    geodesic: true,
-    map: map
-  });
-  
-  var map = new google.maps.Map(document.getElementById('pickupmap2'), {
-    zoom: 8,
-    center: {
-      lat: 37.8590937,
-      lng: -122.4852507
-    },
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-  var infowindow = new google.maps.InfoWindow();
-  var marker, i;
-  for (i = 0; i < locations.length; i++) {
-    marker = new google.maps.Marker({
-      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-      map: map,
-      icon: locations[i][3]
-    });
-    google.maps.event.addListener(marker, 'click', (function (marker, i) {
-      return function () {
-        infowindow.setContent(locations[i][0]);
-        infowindow.open(map, marker);
-      }
-    })(marker, i));
-  }
-  var line = new google.maps.Polyline({
-    path: [new google.maps.LatLng(37.8590937, -122.4852507), new google.maps.LatLng(38.5815719, -121.4943996)],
-    strokeColor: "#00a000",
-    strokeOpacity: 1.0,
-    strokeWeight: 5,
-    geodesic: true,
-    map: map
-  });
+(function () {
+    "use strict";
+
+    // Google Map api v3 - Map for contact pages
+    if ( document.getElementById("map") && typeof google === "object" ) {
+        var content = '<address>' +
+                            '123 Street Name, City,<br>' +
+                            '1001 United States<br>'+
+                            '<a href="#">Get Directions <i class="icon-angle-right"></i></a>'+
+                        '</address>';
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
+            center: new google.maps.LatLng(-34.398, 150.884), // Map Center coordinates
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            styles: [{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#d3d3d3"}]},{"featureType":"transit","stylers":[{"color":"#808080"},{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"color":"#b3b3b3"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"weight":1.8}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#d7d7d7"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ebebeb"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#a7a7a7"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#efefef"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#696969"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"color":"#737373"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#d6d6d6"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#dadada"}]}]
+
+        });
+
+        var infowindow = new google.maps.InfoWindow({
+            content: content,
+            maxWidth: 360
+        });
+
+        var marker;
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(-34.398, 150.884),
+            map: map,
+            animation: google.maps.Animation.DROP
+        });
+
+        google.maps.event.addListener(marker, 'click', (function (marker) {
+            return function() {
+              infowindow.open(map, marker);
+            }
+        })(marker));
+    }
+})();
